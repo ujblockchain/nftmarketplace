3f8d0034d7a103effea1b431abde9a62e74f3ad6
edmundrotimi <edmundrotimi@gmail.com>
2023-08-28 13:56:02 +0200

    run server

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/ujblockchain/nftmarketplace/server"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	//init echo
+	router := echo.New()
+
+	router.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	// listen and serve on 0.0.0.0:8080
+	server.RunServer(router)
 }
